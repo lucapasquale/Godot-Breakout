f70ba7b4f39cd33b0266a7aca8e6f9d549472adf
Luca Pasquale <lucapasquale.094@gmail.com>
2018-04-02 23:00:56 -0300

    Added score label

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -1,24 +1,30 @@
 extends RigidBody2D
 
-const SPEEDUP = 4
-const MAX_SPEED = 300
+export(int) var speedup = 4
+var max_speed = 300
 
 
 func _physics_process(delta):
 	var bodies = get_colliding_bodies()
-	
 	for body in bodies:
 		if body.is_in_group("Bricks"):
-			body.queue_free()
-			
+			hit_brick(body)
+
 		if body.name == "Paddle":
-			var current_speed = get_linear_velocity().length()
-			var speed = min(current_speed + SPEEDUP, MAX_SPEED)
-			
-			var current_direction = get_position() - body.get_node("Anchor").get_global_position()
-			var velocity = current_direction.normalized() * speed
-			set_linear_velocity(velocity)
-			
+			hit_paddle(body)
+
 	if get_position().y > get_viewport_rect().end.y:
-		print("Ball died")
 		queue_free()
+
+
+func hit_brick(body):
+	get_node("/root/World").score += 5
+	body.queue_free()
+	
+func hit_paddle(body):
+	var current_speed = get_linear_velocity().length()
+	var speed = min(current_speed + speedup, max_speed)
+	
+	var current_direction = get_position() - body.get_node("Anchor").get_global_position()
+	var velocity = current_direction.normalized() * speed
+	set_linear_velocity(velocity)
